c55cdc881780ba619dd82fd7b4b063ff147f2f63
Valeriya Rabinchuk <lerumpochka@gmail.com>
2022-12-09 17:49:44 +0100

    last changes

--- pages/index.js
+++ pages/index.js
@@ -28,17 +28,8 @@ export default function Home(props) {
 
   export async function getServerSideProps(req, res) {
     const session = await getSession(req);
-    if (!session) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: "/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F",
-        },
-      };
-    }
-
-
+    
   return {
-    props: { currentUser: session },
+    props: { currentUser: session  },
   };
 }
