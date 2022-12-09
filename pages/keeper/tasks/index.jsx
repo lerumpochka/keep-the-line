bc7149e0ac5ae8e09817d67e9b9622130a2758aa
Valeriya Rabinchuk <lerumpochka@gmail.com>
2022-12-09 16:19:44 +0100

    gs del

--- pages/keeper/tasks/index.jsx
+++ pages/keeper/tasks/index.jsx
@@ -1,4 +1,4 @@
-import { getSession } from "next-auth/react";
+
 import React from "react";
 import TasksAroundKeeper from "../../../components/TasksAroundKeeper";
 import { getSession } from "next-auth/react";
