21dc9db841b5557eab69c4185c48b2df614e578d
Fadi Albitar <alabitaref@gmail.com>
2022-12-10 21:05:59 +0100

    PWA FOR KEEP THE LINE

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [
-      'res.cloudinary.com'
-    ]
-  }
+const withPWA = require('next-pwa')({
+ dest: 'public'
+})
 
-}
+const nextConfig = withPWA({
+ // all other next config...
+ reactStrictMode: true,
+ images: {
+   domains: [
+     'res.cloudinary.com'
+   ]
+ }
+})
 
 module.exports = nextConfig
