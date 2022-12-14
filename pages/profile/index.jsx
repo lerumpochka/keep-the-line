f0cf30f361b9376714bfe4a0b4d070e108a36728
Monir <ahmadmonirosmani@gmail.com>
2022-12-14 14:14:04 +0100

    front-end upadted

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,15 +1,24 @@
 import { getSession } from "next-auth/react";
 import React from "react";
 import db from "../../database";
+import styles from "../../styles/Profile.module.css";
+import Link from "next/link";
 // import db from "../../database";
 
 function Profile(props) {
-  console.log(props.currentUser)
+  console.log(props.currentUser);
   return (
     <div>
-      <h1>profile page</h1>
-      <p>{props.currentUser.name}</p>
-      <p>{props.currentUser.email}</p>
+      <div>
+        <h1>profile page</h1>
+        <p>{props.currentUser.name}</p>
+        <p>{props.currentUser.email}</p>
+      </div>
+
+      <div className={styles.buttons__con}>
+        <Link href="/keeper/tasks">Keep a spot</Link>
+        <Link href="/taker/tasks/new">Take a spot</Link>
+      </div>
     </div>
   );
 }
@@ -25,11 +34,13 @@ export async function getServerSideProps(req, res) {
       },
     };
   }
-// find user from DB based on email 
-const user = JSON.parse(JSON.stringify(await db.User.findOne({ where: { email: session.user.email } })))
-return {
-  props: { currentUser: user },
-};
+  // find user from DB based on email
+  const user = JSON.parse(
+    JSON.stringify(await db.User.findOne({ where: { email: session.user.email } }))
+  );
+  return {
+    props: { currentUser: user },
+  };
 }
 
 export default Profile;
--- pages/taker/tasks/index.jsx
+++ pages/taker/tasks/index.jsx
@@ -2,39 +2,57 @@ import { getSession } from "next-auth/react";
 import React from "react";
 import CreatedTaskCard from "../../../components/CreatedTaskCard";
 import db from "../../../database";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import styles from "../../../styles/TakerTasks.module.css";
+import Link from "next/link";
 
 function TakerTasks(props) {
   const tasks = props.tasks;
-  const user = props.currentUser.user
+  const user = props.currentUser.user;
   return (
-    <div>
-      <h1>Created Tasks by {user.email} </h1>
+    <div className={styles.container}>
+      <div className={styles.text__con}>
+        <h1>Created Tasks </h1>
+        <Link href="/taker/tasks/new">
+          <div className={styles.icon__con}>
+            <AddOutlinedIcon sx={{ color: "white", fontSize: "2rem" }} /> <span>New Task</span>
+          </div>
+        </Link>
+      </div>
       {tasks.map((task) => (
-        <CreatedTaskCard key={task.id} id={task.id} title= {task.title} description= {task.description} 
-        address= {task.address} amount= {task.amount} date= {task.date} time= {task.time}/>
-        ))}
+        <CreatedTaskCard
+          key={task.id}
+          id={task.id}
+          title={task.title}
+          description={task.description}
+          address={task.address}
+          amount={task.amount}
+          date={task.date}
+          time={task.time}
+        />
+      ))}
     </div>
   );
 }
 export async function getServerSideProps(req, res) {
-    const session = await getSession(req);
-    if (!session) {
-      return {
-        redirect: {
-          permanent: false,
-          destination: "/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F",
-        },
-      };
-    }
-
-    const userEmail = session.user.email;
-    const user = JSON.parse(JSON.stringify(await db.User.findOne({ where: { email: userEmail } })));
-    const userId = user.id;
-    const tasks = JSON.parse(JSON.stringify(await db.Task.findAll({ where: { UserId: userId } })));
-
+  const session = await getSession(req);
+  if (!session) {
     return {
-      props: { tasks, currentUser: session },
+      redirect: {
+        permanent: false,
+        destination: "/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2F",
+      },
     };
+  }
+
+  const userEmail = session.user.email;
+  const user = JSON.parse(JSON.stringify(await db.User.findOne({ where: { email: userEmail } })));
+  const userId = user.id;
+  const tasks = JSON.parse(JSON.stringify(await db.Task.findAll({ where: { UserId: userId } })));
+
+  return {
+    props: { tasks, currentUser: session },
+  };
 }
 
 export default TakerTasks;
