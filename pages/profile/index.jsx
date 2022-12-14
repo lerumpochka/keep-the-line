f33309373f86ab376c98c4dd4605ae53e9e3871a
Valeriya Rabinchuk <lerumpochka@gmail.com>
2022-12-14 13:51:03 +0100

    profile with btns

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,16 +1,38 @@
 import { getSession } from "next-auth/react";
+import Link from "next/link";
 import React from "react";
 import db from "../../database";
+import styles from "../../styles/HomePage.module.css";
+
+
 // import db from "../../database";
 
 function Profile(props) {
   console.log(props.currentUser)
   return (
-    <div>
-      <h1>profile page</h1>
-      <p>{props.currentUser.name}</p>
-      <p>{props.currentUser.email}</p>
-    </div>
+    <>
+      <div>
+        <h1>Hey {props.currentUser.name}</h1>
+        <br />
+        <br />
+
+
+        <h3> Booked Tasks:</h3>
+        <br />
+        <br />
+        <div className={styles.buttons__con}>
+          <Link href="/keeper/tasks/booked">Booked Tasks</Link>
+        </div>
+        <h3> Created Tasks:</h3>
+        <br />
+        <br />
+        <div className={styles.buttons__con}>
+          <Link href="/taker/tasks/">My Tasks</Link>
+        </div>
+
+
+      </div>
+    </>
   );
 }
 
@@ -25,11 +47,11 @@ export async function getServerSideProps(req, res) {
       },
     };
   }
-// find user from DB based on email 
-const user = JSON.parse(JSON.stringify(await db.User.findOne({ where: { email: session.user.email } })))
-return {
-  props: { currentUser: user },
-};
+  // find user from DB based on email 
+  const user = JSON.parse(JSON.stringify(await db.User.findOne({ where: { email: session.user.email } })))
+  return {
+    props: { currentUser: user },
+  };
 }
 
 export default Profile;
